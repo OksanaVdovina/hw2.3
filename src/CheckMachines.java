3d339a7d4b3c017075d639000cbffb4197bbe4bc
Oksana Vdovina <oksanavdovina@inbox.ru>
2022-07-13 17:21:45 +0700

    hw 2.3

---
+++ src/CheckMachines.java
@@ -0,0 +1,5 @@
+public interface CheckMachines {
+    void check();
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CheckMachines car = new Car("car1",4);
+        CheckMachines car2 = new Car("car2", 4);
+        CheckMachines truck = new Truck("truck1",6);
+        CheckMachines truck2 = new Truck("truck2", 8);
+        CheckMachines bicycle = new Bicycle("bicycle1",2);
+        CheckMachines bicycle2 = new Bicycle("bicycle2",2);
+
+        car.check();
+        System.out.println();
+        truck.check();
+        System.out.println();
+        bicycle.check();
+        System.out.println();
+
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.verify(car);
+
     }
 }
